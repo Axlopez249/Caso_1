b736d749e5d2d6a7740a6667702cbf54b0a101d0
Axel Lopez <axlopez@estudiantec.cr>
2023-08-25 09:34:27 -0600

    Preliminar 1

---
+++ Preliminar_1/Preliminar1_OP.h
@@ -0,0 +1,15 @@
+//Axel López Vega
+//Lista sencilla doblemente enlazada
+
+//Operaciones a realizar para hacer uso de las 4 funciones principales
+
+tomar5(Datos lista_info);
+//Para tomar la prioridad busca nodo por nodo con los datos seleccionados
+tomarLista();
+//Retorna todos los elementos de la lista. usa la funcion de mostrar nodo siguiente en un ciclo
+eliminar_con_info(Datos lista_info);
+//Elimina y reacomoda los nodos que tienen esa información. Usa mostrar_nodo_siguiente y eliminar_nodo
+subir_bajar(Dato dato, int prioridad);
+//sube la prioridad. Busca el dato, obtiene la posi del nodo, Busca el key del nodo de esa prioridad y se ubica atrás de él
+//Luego el que estaba ahí se puede eliminar y ponerlo en otro lado o reacomodarlo conectando el que tengo con el que tenía con el siguiente y el anterior
+ 
---
+++ Preliminar_1/Preliminar1_TAD.h
@@ -0,0 +1,25 @@
+//Axel López Vega 2023094841
+//Lista utilizada la "doblemente enlazada"
+
+//Métodos del TAD a utilizar
+void insertar_cabeza(Dato dato);
+    //Ingresa un dato al inicio de la lista
+void insertar_con_referencia(node nodo_antes);
+    // Inserta un dato donde se encuentra otro
+void buscar_posi_nodo(Dato dato);
+    //busca un dato. Psoiblemente retornaría la ubicacion donde está el nodo
+void eliminar_nodo(Dato dato);
+    //Busca un dato y lo elimina
+void mostrar_info_nodo(node nodo_referencia);
+    //Muestra la informacion del nodo
+void mostrar_nodo_siguiente(node nodo_referencia);
+    //Muestra el siguiente nodo de un nodo de referencia (para cuando muestra toda la lista)
+void mostras_nodo_anterior(node nodo_referencia);
+    //Muestra el nodo anterior de un nodo de referencia (para reubicar)
+
+
+
+
+
+
+ 
